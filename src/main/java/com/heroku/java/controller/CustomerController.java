6373cb7bb3e6b403ef5851511f74cf016628b99b
ASYRAFSAMA <2022862742@student.uitm.edu.my>
2024-06-29 04:36:41 +0800

    2

--- src/main/java/com/heroku/java/controller/CustomerController.java
+++ src/main/java/com/heroku/java/controller/CustomerController.java
@@ -31,6 +31,45 @@ public class CustomerController {
 
     @PostMapping("/register")
     public String registerCustomer(@ModelAttribute("customer") Customer customer, Model model) {
+
+        try {
+            Connection connection = dataSource.getConnection();
+            String sql = "INSERT INTO public.guest(guesticnumber, guestname, guestphonenumber, guestgender, guestreligion, guestrace, guestaddress, guestemail, guestpassword) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+            final var statement = connection.prepareStatement(sql);
+
+            String CustomerName = customer.getCustomerName();
+            String CustomerDob = customer.getCustomerDob();
+            String CustomerEmail = customer.getCustomerEmail();
+            String CustomerPhoneNum = customer.getCustomerPhoneNum();
+            String CustomerAddress = customer.getCustomerAddress();
+            String Password = customer.getPassword();
+
+
+            statement.setString(1, customer.getCustomerName());
+            statement.setString(2, customer.getCustomerDob());
+            statement.setString(3, customer.getCustomerEmail());
+            statement.setString(4, customer.getCustomerPhoneNum());
+            statement.setString(5, customer.getCustomerAddress());
+            statement.setString(6, customer.getPassword());
+            statement.executeUpdate();
+
+
+
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/index";
+        }
+        return "redirect:/guestLogin";
+    }
+}
+
+
+
+/* 
+    @PostMapping("/register")
+    public String registerCustomer(@ModelAttribute("customer") Customer customer, Model model) {
         try (Connection connection = dataSource.getConnection()) {
             String sql = "INSERT INTO customer (customername, customerdob, customeremail, customerphonenum, customeraddress, password) VALUES (?, ?, ?, ?, ?, ?)";
             PreparedStatement statement = connection.prepareStatement(sql);
@@ -40,13 +79,40 @@ public class CustomerController {
             statement.setString(4, customer.getCustomerPhoneNum());
             statement.setString(5, customer.getCustomerAddress());
             statement.setString(6, customer.getPassword());
+            
+//sini
+    final var resultSet = statement.executeQuery();
 
-            statement.executeUpdate();
+
+
+if (resultSet.next()) {
+
+    String customerName = resultSet.getString("CustomerName");
+    String customerEmail = resultSet.getString("customerEmail");
+    String customerDob = resultSet.getString("customerDob");
+    String customerPhoneNum = resultSet.getString("customerPhoneNum");
+    String customerAddress = resultSet.getString("customerAddress");
+    String Password = resultSet.getString("Password");
+
+}
+
+connection.close();
+return "redirect:/guestLogin?invalidUsername&Password";
+
+
+
+
+//sini
+
+            resultSet.executeUpdate();
             return "redirect:/login";
         } catch (SQLException e) {
             e.printStackTrace();
             model.addAttribute("error", "An error occurred while registering the customer.");
             return "register";
         }
+        
     }
 }
+
+*/
--- src/main/java/com/heroku/java/model/CustomerModel.java
+++ src/main/java/com/heroku/java/model/CustomerModel.java
@@ -1,3 +1,5 @@
+/* 
+
 package com.heroku.java.model;
 
 public class CustomerModel {
@@ -59,3 +61,5 @@ public class CustomerModel {
         this.password = password;
     }
 }
+
+*/
