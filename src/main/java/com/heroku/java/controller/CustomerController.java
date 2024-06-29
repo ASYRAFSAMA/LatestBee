d881125b32099551860f5c86443e018347db812f
ASYRAFSAMA <2022862742@student.uitm.edu.my>
2024-06-30 01:08:45 +0800

    1

--- src/main/java/com/heroku/java/controller/CustomerController.java
+++ src/main/java/com/heroku/java/controller/CustomerController.java
@@ -29,7 +29,7 @@ public class CustomerController {
         this.dataSource = dataSource;
     }
 
-    @PostMapping("/register")
+    @PostMapping("/registerAccount")
     public String register(@ModelAttribute("register")
                                 @RequestParam("customername") String customerName,
                                 @RequestParam("customerdob") String customerDob,
